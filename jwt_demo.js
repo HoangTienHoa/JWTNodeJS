c1a34057981a845cd6f7de71f7160efdd33a27ba
Hoang Tien Hoa <hoangtienhoa@Hoangs-MacBook-Pro.local>
2025-08-12 23:33:52 +0700

    update alg

--- jwt_demo.js
+++ jwt_demo.js
@@ -1,6 +1,7 @@
 const JWT = require('jsonwebtoken');
 const keySecret = 'ThiIsVeryStrongSecretKey';
-const token= JWT.sign({userId:123, roles:['admin']},keySecret, {expiresIn: '1h'});
+const token= JWT.sign({userId:123, roles:['admin']},keySecret, {algorithm:'HS256', expiresIn: '1h'});
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyMywicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNzU1MDE2MzQxLCJleHAiOjE3NTUwMTk5NDF9.XZT6NUl3GFl6rQ9BJ-q4irHcYXucf4BfYvtIENhQfnU
 console.log('token: ',token);
 JWT.verify(token,keySecret, (err, decoded) => {
     if (err) console.log('Error verify token',err);
